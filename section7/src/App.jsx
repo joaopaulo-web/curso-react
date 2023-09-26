2746938c428b6f8d4479e3d2f712df8dc578fc18
JoaoPaulo <joaopaulocoimbra2013@gmail.com>
2023-09-26 07:39:50 -0300

    FInalização da seção 7

--- section7/src/App.jsx
+++ section7/src/App.jsx
@@ -1,27 +1,36 @@
 import './App.css'
 import {useState, useEffect} from "react";
 
+// 4 Custom Hook
+
+import {useFetch} from "./hooks/useFetch.js";
+
 const url = "http://localhost:3000/products"
 
 function App() {
 
     const [products, setProducts] = useState([])
+
+    // 4 Custom hook
+
+    const {data: items, httpConfig, loading, error} = useFetch(url)
+
     const [name, setName] = useState("")
     const [price, setPrice] = useState("")
 
     //Resgatando os dados
-    useEffect(() => {
-        async function fetchData(){
-
-            const res = await fetch (url)
-
-            const data = await res.json()
-
-            setProducts(data)
-        }
-
-        fetchData()
-    }, []);
+    // useEffect(() => {
+    //     async function fetchData(){
+    //
+    //         const res = await fetch (url)
+    //
+    //         const data = await res.json()
+    //
+    //         setProducts(data)
+    //     }
+    //
+    //     fetchData()
+    // }, []);
 
 
 
@@ -38,25 +47,51 @@ function App() {
 
         //Este bloco faz uma solicitação HTTP utilizando a função fetch. Ela envia uma solicitação POST para o URL especificado na variável url.
         //O await é usado para esperar que a solicitação seja concluída antes de prosseguir com a execução do código. Como a função handleSubmit é assíncrona, o uso de await permite que a função aguarde a conclusão da solicitação antes de continuar.
-        const res = await fetch(url, {
-            method: "POST",
-            headers: {
-                "Content-Type":"application/json"
-            },
-            body: JSON.stringify(product),
-        })
+        // const res = await fetch(url, {
+        //     method: "POST",
+        //     headers: {
+        //         "Content-Type":"application/json"
+        //     },
+        //     body: JSON.stringify(product),
+        // })
+        //
+        // // 3 - Carregamento dinâmico
+        //
+        // const addedProduct = await res.json()
+        //
+        // setProducts((prevProducts) => [...prevProducts, addedProduct])
 
+        // 5 - Refatorando POST
+        httpConfig(product, "POST")
+
+        setName("")
+        setPrice("")
+    }
+
+    //desafio 6
+
+    const handleRemove = (id) => {
+        httpConfig(id, 'DELETE')
     }
 
   return (
     <>
       <h1>Lista de Produtos</h1>
-        <ul>
-            {products.map((product) => (
-                <li key={product.id}>{product.name} - R$: {product.price}</li>
-            ))}
-            {/*Parte do código responsável por mapear cada elemento da string e tornar uma nova string. Diferenciando cada nova string por um valor único (id). Cada novo valor vai ser disposto em uma concatenação de li com o valor do nome e do preço.*/}
-        </ul>
+        {/*6 - Loading*/}
+        {loading && <p> Carregando dados...</p>}
+        {error && <p>{error}</p>}
+        {!error && (
+            <ul>
+                {items &&
+                    items.map((product) => (
+                    <li key={product.id}>
+                        {product.name} - R$: {product.price}
+                        <button onClick={() => handleRemove(product.id)}>Excluir</button>
+                    </li>
+                ))}
+                {/*Parte do código responsável por mapear cada elemento da string e tornar uma nova string. Diferenciando cada nova string por um valor único (id). Cada novo valor vai ser disposto em uma concatenação de li com o valor do nome e do preço.*/}
+            </ul>
+        )}
         <div className="add-product">
             <form onSubmit={handleSubmit}>
                 <label>
@@ -73,7 +108,9 @@ function App() {
                            name="price"
                            onChange={(e) => setPrice(e.target.value)}/>
                 </label>
-                <input type="submit" value="Criar"/>
+                {/*7 - State de loading no post*/}
+                {loading && <input type="submit" disabled value="Aguarde"/>}
+                {!loading && <input type="submit" value="Criar"/>}
             </form>
         </div>
     </>
