9ad1755dda4a4eb6ba2a1c2a95787bcd4888e66d
JoaoPaulo <joaopaulocoimbra2013@gmail.com>
2023-10-04 08:15:25 -0300

    Finalização da seção 8

--- section8/src/App.jsx
+++ section8/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 
 
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 
 // Pages
 import Home from "./pages/Home.jsx";
@@ -12,6 +12,8 @@ import Navbar from "./components/Navbar.jsx"
 import Product from "./pages/Product.jsx";
 import Info from "./pages/Info.jsx";
 import NotFound from "./pages/NotFound.jsx";
+import SearchForm from "./components/SearchForm.jsx";
+import Search from "./pages/Search.jsx";
 
 function App() {
 
@@ -21,6 +23,8 @@ function App() {
             <h1>React Router</h1>
             <BrowserRouter>
                 <Navbar />
+                {/*Search*/}
+                <SearchForm></SearchForm>
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/about" element={<About />} />
@@ -28,6 +32,10 @@ function App() {
                     <Route path="/products/:id/info" element={<Info />} />
                     {/*Rota dinâmica*/}
                     <Route path="/products/:id" element={<Product />} />
+                    {/*Search*/}
+                    <Route path="/search" element={<Search />} />
+                    {/*Redirect*/}
+                    <Route path="/company" element={<Navigate to="/about" />} />
                     {/*No Match Route*/}
                     <Route path = "*" element={<NotFound />}/>
                 </Routes>
--- section8/src/components/Navbar.jsx
+++ section8/src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import './Navbar.css'
-import {Link} from "react-router-dom";
+import {Link, NavLink} from "react-router-dom";
 const Navbar = () => {
     return (
         <nav>
-            <Link to="/">Home</Link>
-            <Link to="/about">Sobre</Link>
+            {/*<Link to="/">Home</Link>
+            <Link to="/about">Sobre</Link>*/}
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/about">Sobre</NavLink>
         </nav>
     )
 }
