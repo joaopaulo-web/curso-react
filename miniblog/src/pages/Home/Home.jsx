17827cf7fd6bc0dc8bab3af8959d010e8ef2093d
JoaoPaulo <joaopaulocoimbra2013@gmail.com>
2023-10-23 15:02:58 -0300

    miniblog

--- miniblog/src/pages/Home/Home.jsx
+++ miniblog/src/pages/Home/Home.jsx
@@ -4,13 +4,14 @@ import styles from './Home.module.css'
 import { useNavigate, Link } from 'react-router-dom'
 import { useState } from 'react'
 import { FieldValue } from 'firebase/firestore/lite'
+import { useFetchDocuments } from '../../hooks/useFetchDocuments'
 
 //components
 
 
 const Home = () => {
   const [query, setQuery] = useState('')
-  const [posts] = useState([])
+  const {documents: posts, loading} = useFetchDocuments('posts')
   
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -23,7 +24,8 @@ const Home = () => {
           <button className="btn btn-dark">Pesquisar</button>
         </form>
         <div>
-          <h1>Postagens</h1>
+          {loading && <p>Carregando...</p>}
+          {posts && posts.map((post) =><h3>{post.document.title}</h3>)}
           {posts && posts.length === 0 &&(
             <div className={styles.noposts}>
               <p>Não foram encontrados posts</p>
