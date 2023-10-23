b501bf86eae78dbc99bfd2a6ec311da5f5f8ea38
JoaoPaulo <joaopaulocoimbra2013@gmail.com>
2023-10-23 09:13:48 -0300

    Mais uma parte do projeto miniblog

--- miniblog/src/hooks/useAuthentication.jsx
+++ miniblog/src/hooks/useAuthentication.jsx
@@ -33,10 +33,9 @@ export const useAuthentication = () => {
     const createUser = async (data) => {
 
         checkIfIsCanceled()
-
-        setLoading(true)
-        
-        setError(null)
+        setCancelled(true);
+        setError(null);
+        setLoading(true);
 
 
         try {
