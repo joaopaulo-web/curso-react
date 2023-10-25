88b13f51e872f64971db3b70cb4a447e2f9d51bf
JoaoPaulo <joaopaulocoimbra2013@gmail.com>
2023-10-25 14:11:40 -0300

    Finalização e adição das redes sociais

--- miniblog/src/components/Footer.jsx
+++ miniblog/src/components/Footer.jsx
@@ -4,8 +4,15 @@ import styles from './Footer.module.css'
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <h3>Espaço reservado para o footer!</h3>
-      <p>Mini Blog &copy; 2023</p>
+      <p>João Paulo Dev &copy; 2023</p>
+      <div className={styles.redes_footer}>
+       <p>
+          <a href="https://www.linkedin.com/in/joao-paulo-web/"> <img src="src/assets/linkedin.svg" alt="Ícone Linkedin"/> Linkedin</a> 
+        </p>
+        <p>
+          <a href="https://github.com/joaopaulo-web"> <img src="src/assets/github.svg" alt="Ícone GitHub"/> GitHub</a>
+        </p>
+      </div>
     </footer>
   )
 }
