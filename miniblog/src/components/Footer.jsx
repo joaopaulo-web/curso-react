4ef0c50a293c294b10c29c48b3128a6e6c90520d
JoaoPaulo <joaopaulocoimbra2013@gmail.com>
2023-10-25 14:20:06 -0300

    teste de push

--- miniblog/src/components/Footer.jsx
+++ miniblog/src/components/Footer.jsx
@@ -5,6 +5,7 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       <p>João Paulo Dev &copy; 2023</p>
+      teste
       <div className={styles.redes_footer}>
        <p>
           <a href="https://www.linkedin.com/in/joao-paulo-web/"> <img src="src/assets/linkedin.svg" alt="Ícone Linkedin"/> Linkedin</a> 
